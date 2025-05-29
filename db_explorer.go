3c3cc01bef2b4be017dfe8b41fbabcb2932dc80d
letnonesurvive <hippiman1221@gmail.com>
2025-05-30 00:29:21 +0300

    First commit

---
+++ db_explorer.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+)
+
+// тут вы пишете код
+// обращаю ваше внимание - в этом задании запрещены глобальные переменные
+
+type DbExplorer struct {
+	db     *sql.DB
+	router *http.ServeMux
+}
+
+func (exp *DbExplorer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	exp.router.ServeHTTP(w, r)
+}
+
+func NewDbExplorer(db *sql.DB) (*DbExplorer, error) {
+	exp := &DbExplorer{db: db, router: http.NewServeMux()}
+
+	exp.router.HandleFunc("/", exp.listFunc)
+
+	return exp, nil
+}
+
+func (exp *DbExplorer) listFunc(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		rows, err := exp.db.Query("SHOW DATABASES;")
+		defer rows.Close()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		for rows.Next() {
+			var name string
+			rows.Scan(&name)
+			fmt.Println(name)
+		}
+	}
+}
